49f0e9513ce1829f983d421d1186db023f73d15c
rehmanghani2 <rehmanghani2323@gmail.com>
2025-03-09 17:58:35 +0500

    First commit

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -18,7 +18,7 @@ export const AppContextProvider = (props) => {
 
     const [products, setProducts] = useState([]);
     const [userData, setUserData] = useState(false);
-    const [isSeller, setIsSeller] = useState(true);
+    const [isSeller, setIsSeller] = useState(false);
     const [cartItems, setCartItems] = useState({});
 
     const fetchProductData = async () => {
@@ -26,7 +26,15 @@ export const AppContextProvider = (props) => {
     }
 
     const fetchUserData = async () => {
-        setUserData(userDummyData);
+        try {
+            if(user.publicMetadata.role === "seller"){
+                setIsSeller(true)
+            }
+            setUserData(userDummyData);
+            
+        } catch (error) {
+            
+        }
     }
 
     const addToCart = async (itemId) => {
@@ -76,8 +84,10 @@ export const AppContextProvider = (props) => {
     }, []);
 
     useEffect(() => {
-        fetchUserData
-    }, []);
+        if(user){
+            fetchUserData()
+        }  
+    }, [user]);
 
     const value = {
         user,
