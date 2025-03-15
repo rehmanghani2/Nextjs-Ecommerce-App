fe4cabc5f350f9b03a8f41e653783edc8ee7eeda
rehmanghani2 <rehmanghani2323@gmail.com>
2025-03-15 23:35:25 +0500

    First commit

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+
 const productSchema = new mongoose.Schema({
     userId: { type: String, required: true, ref: "user"},
     name: { type: String, required: true },
