73fa863ee8f60b30ddc6a9b94454bbb3dbdc470b
rehmanghani2 <rehmanghani2323@gmail.com>
2025-03-15 22:45:07 +0500

    First commit

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true},
-    offerPrice: { type: Number, required: type},
+    offerPrice: { type: Number, required: true},
     image: { type: Array, required: true},
     category: { type: String, required: true},
     date: { type: Number, required: true }
